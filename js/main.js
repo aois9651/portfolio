c48800dfae61003712afbf54d7ab92a7a9bb9164
aois9651 <167090294+aois9651@users.noreply.github.com>
2024-05-10 15:32:36 +0900

    comment

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+$(function(){
+  $('.menu-trigger').on('click', function(){
+    $(this).toggleClass('active');
+    $("#g-nav").toggleClass('panelactive');
+    return false;
+  });
+
+  $("#g-nav a").click(function(){
+    // $(".menu-trigger").removeClass('active');
+    $("#g-nav").removeClass('panelactive');
+  });
+});
+
 
