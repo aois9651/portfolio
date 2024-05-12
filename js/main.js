61bc805ea9fb4af08f4ec3e0278ae619f09c524a
aois9651 <167090294+aois9651@users.noreply.github.com>
2024-05-12 21:38:42 +0900

    comment

--- js/main.js
+++ js/main.js
@@ -11,4 +11,20 @@ $(function(){
   });
 });
 
-
+var beforePos = 0;
+function ScrollAnime(){
+  var elemTop = $('#area-1').offset().top;
+  var scroll = $(window).scrollTop();
+  if(scroll == beforePos){
+  }else if(elemTop > scroll || 0 > scroll - beforePos){
+    $('.small-header').removeClass('UpMove');
+    $('.small-header').addClass('DownMove');
+  }else{
+    $('.small-header').removeClass('DownMove');
+    $('.small-header').addClass('UpMove')
+  }
+  beforePos = scroll;
+}
+$(window).scroll(function(){
+  ScrollAnime();
+});
