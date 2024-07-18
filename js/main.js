123949468b22d2055814486fa7763cedc7d2286c
aois9651 <167090294+aois9651@users.noreply.github.com>
2024-07-18 15:21:01 +0900

    comment

--- js/main.js
+++ js/main.js
@@ -209,12 +209,34 @@ function fadeAnime() {
     }
   });
 }
+
 $(window).scroll(function (){
   fadeAnime();
 });
 
 $(window).on('load', function(){
   delayScrollAnime();
+});
+
+function fadeAnime2() {
+  $('.rotateTrigger').each(function () {
+    var elemPos = $(this).offset().top + 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('rotateY');
+    } else {
+      $(this).removeClass('rotateY');
+    }
+  });
+}
+
+$(window).scroll(function (){
+  fadeAnime2();
+});
+
+$(window).on('load', function(){
+  delayScrollAnime2();
 });
 
 // Emblem
